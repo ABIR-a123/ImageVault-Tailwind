71a647e6bb95ea40f2804b141eb21bbb332ba259
Abir Hossain <60575695+ABIR-a123@users.noreply.github.com>
2023-05-31 13:11:10 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      margin: {
+        '13': "30rem",
+      },
+      inset: {
+        '26': '6.5rem',
+      }
+    },
+  },
+  plugins: [],
+};
